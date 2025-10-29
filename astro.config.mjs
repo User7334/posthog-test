8770bdcf0b0ecfc11783d4989977d973fdbf4740
User7334 <lukas123-hartmann@web.de>
2025-10-29 14:11:21 +0100

    Update config to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  adapter: vercel(),
+  output: 'static',
   vite: {
     plugins: [tailwindcss()]
   }
