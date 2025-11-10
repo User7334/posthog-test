9cbc7ad209ab0ba17c3af871b4cebd0701cbf19b
User7334 <lukas123-hartmann@web.de>
2025-11-10 19:03:27 +0100

    Only execute posthog-init on Browser

--- src/scripts/posthog-init.ts
+++ src/scripts/posthog-init.ts
@@ -1,7 +1,8 @@
-import { initPosthog } from "./posthog";
-import "./abtest";
-
-const key = import.meta.env.PUBLIC_POSTHOG_KEY || "phc_KOcWjzbh56k1BfKTHlPJD0PGavdy7gMd7qVME4syB40";
-const host = import.meta.env.PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com";
-
-initPosthog(key, host);
+if (typeof window !== "undefined") {
+  import("./posthog").then(({ initPosthog }) => {
+    import("./abtest");
+    const key = import.meta.env.PUBLIC_POSTHOG_KEY || "phc_KOcWjzbh56k1BfKTHlPJD0PGavdy7gMd7qVME4syB40";
+    const host = import.meta.env.PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com";
+    initPosthog(key, host);
+  });
+}
